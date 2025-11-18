135d296f7234a9081064ffb053eb408640603584
Valerii Korniienko <myksv200@gmail.com>
2025-11-18 10:14:38 +0200

    :hammer: Basic database copying

---
+++ src/TestingTask.CLI/MainExecutionThread.cs
@@ -0,0 +1,162 @@
+﻿using System.Data;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace TestingTask.CLI;
+
+public class Foo
+{
+    public DateTime tpep_pickup_datetime { get; set; }
+    public DateTime tpep_dropoff_datetime { get; set; }
+    public int passenger_count { get; set; }
+    public decimal trip_distance { get; set; }
+    public string? store_and_fwd_flag { get; set; }
+    public int PULocationID { get; set; }
+    public int DOLocationID { get; set; }
+    public decimal fare_amount { get; set; }
+    public decimal tip_amount { get; set; }
+}
+
+public class MainExecutionThread(AppSettings appSettings)
+{
+    public void Run()
+    {
+        WriteDuplicatesCsv(appSettings);
+
+        var foos = ReadCsvFile(appSettings);
+
+        var table = CreateDataTableSchema();
+
+        foreach (var foo in foos)
+        {
+            var row = table.NewRow();
+            row["tpep_pickup_datetime"] = foo.tpep_pickup_datetime;
+            row["tpep_dropoff_datetime"] = foo.tpep_dropoff_datetime;
+            row["passenger_count"] = foo.passenger_count;
+            row["trip_distance"] = foo.trip_distance;
+            row["store_and_fwd_flag"] = foo.store_and_fwd_flag;
+            row["PULocationID"] = foo.PULocationID;
+            row["DOLocationID"] = foo.DOLocationID;
+            row["fare_amount"] = foo.fare_amount;
+            row["tip_amount"] = foo.tip_amount;
+            
+            table.Rows.Add(row);
+        }
+
+        BulkInsertTable(table, appSettings);
+
+        static List<Foo> ReadCsvFile(AppSettings appSettings)
+        {
+            using var reader = new StreamReader(appSettings.InputFilePath);
+            var csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                IgnoreBlankLines = true,
+                TrimOptions = TrimOptions.Trim,
+                BadDataFound = null,
+                MissingFieldFound = null
+            };
+
+            using var csv = new CsvReader(reader, csvConfiguration);
+            List<Foo> records = new();
+            csv.Read();
+            csv.ReadHeader();
+            while (csv.Read())
+            {
+                csv.TryGetField<DateTime>("tpep_pickup_datetime", out var tpepPickupDatetime);
+                csv.TryGetField<DateTime>("tpep_dropoff_datetime", out var tpepDropoffDatetime);
+                csv.TryGetField<int>("passenger_count", out var passengerCount);
+                csv.TryGetField<decimal>("trip_distance", out var tripDistance);
+                csv.TryGetField<string>("store_and_fwd_flag", out var storeAndFwdFlag);
+                csv.TryGetField<int>("PULocationID", out var pulocationId);
+                csv.TryGetField<int>("DOLocationID", out var dolocationId);
+                csv.TryGetField<decimal>("fare_amount", out var fareAmount);
+                csv.TryGetField<decimal>("tip_amount", out var tipAmount);
+
+                var record = new Foo()
+                {
+                    tpep_pickup_datetime = tpepPickupDatetime,
+                    tpep_dropoff_datetime = tpepDropoffDatetime,
+                    passenger_count = passengerCount,
+                    trip_distance = tripDistance,
+                    store_and_fwd_flag = storeAndFwdFlag,
+                    PULocationID = pulocationId,
+                    DOLocationID = dolocationId,
+                    fare_amount = fareAmount,
+                    tip_amount = tipAmount
+                };
+                records.Add(record);
+            }
+
+            return records;
+        }
+
+        static void WriteDuplicatesCsv(AppSettings appSettings)
+        {
+            using var duplicatesWriter = new StreamWriter(appSettings.DuplicatesFilePath, false);
+            using var duplicatesCsv = new CsvWriter(duplicatesWriter, CultureInfo.InvariantCulture);
+
+            duplicatesCsv.WriteField("tpep_pickup_datetime");
+            duplicatesCsv.WriteField("tpep_dropoff_datetime");
+            duplicatesCsv.WriteField("passenger_count");
+            duplicatesCsv.WriteField("trip_distance");
+            duplicatesCsv.WriteField("store_and_fwd_flag");
+            duplicatesCsv.WriteField("PULocationID");
+            duplicatesCsv.WriteField("DOLocationID");
+            duplicatesCsv.WriteField("fare_amount");
+            duplicatesCsv.WriteField("tip_amount");
+            duplicatesCsv.NextRecord();
+        }
+
+        static DataTable CreateDataTableSchema()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add(new DataColumn("tpep_pickup_datetime", typeof(DateTime)));
+            dt.Columns.Add(new DataColumn("tpep_dropoff_datetime", typeof(DateTime)));
+            dt.Columns.Add(new DataColumn("passenger_count", typeof(byte)));
+            dt.Columns.Add(new DataColumn("trip_distance", typeof(decimal)));
+            dt.Columns.Add(new DataColumn("store_and_fwd_flag", typeof(string)));
+            dt.Columns.Add(new DataColumn("PULocationID", typeof(int)));
+            dt.Columns.Add(new DataColumn("DOLocationID", typeof(int)));
+            dt.Columns.Add(new DataColumn("fare_amount", typeof(decimal)));
+            dt.Columns.Add(new DataColumn("tip_amount", typeof(decimal)));
+            return dt;
+        }
+
+        static int BulkInsertTable(DataTable table, AppSettings appSettings)
+        {
+            using var conn = new SqlConnection(appSettings.ConnectionString);
+            conn.Open();
+            // return 1;
+            using var bulk = new SqlBulkCopy(conn)
+            {
+                DestinationTableName = "dbo.Trips",
+                BatchSize = table.Rows.Count,
+                BulkCopyTimeout = 600
+            };
+
+            bulk.ColumnMappings.Add("tpep_pickup_datetime", "tpep_pickup_datetime");
+            bulk.ColumnMappings.Add("tpep_dropoff_datetime", "tpep_dropoff_datetime");
+            bulk.ColumnMappings.Add("passenger_count", "passenger_count");
+            bulk.ColumnMappings.Add("trip_distance", "trip_distance");
+            bulk.ColumnMappings.Add("store_and_fwd_flag", "store_and_fwd_flag");
+            bulk.ColumnMappings.Add("PULocationID", "PULocationID");
+            bulk.ColumnMappings.Add("DOLocationID", "DOLocationID");
+            bulk.ColumnMappings.Add("fare_amount", "fare_amount");
+            bulk.ColumnMappings.Add("tip_amount", "tip_amount");
+
+            try
+            {
+                bulk.WriteToServer(table);
+                return table.Rows.Count;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bulk insert failed: " + ex.Message);
+                throw;
+            }
+        }
+    }
+}
--- src/TestingTask.CLI/MyDbContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace TestingTask.CLI;
-
-public class MyDbContext(DbContextOptions<MyDbContext> options) : DbContext(options) {
-    public DbSet<Product> Products { get; set; }
-}
